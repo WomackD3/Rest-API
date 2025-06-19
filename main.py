f0c3798d341f620d76c9ccaa3b3ff2def96f879a
Mack <D_wom3@yahoo.com>
2025-06-19 02:29:45 -0400

    testing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, request
-from flask_restful import Resources, Api, abort, reqparse
+from flask import Flask
+from flask_restful import Resource, Api, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+    
+api.add_resource(HelloWorld, "/helloworld")
 
 if __name__ == "__main__":
     app.run(debug=True)
