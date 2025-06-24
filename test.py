49abb2a6cbc21f2d3cbdb2fe494324ddb257b534
Mack <D_wom3@yahoo.com>
2025-06-24 11:09:25 -0400

    handling delete, database configurations, serializing objects, and adding entries to database

--- test.py
+++ test.py
@@ -2,8 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-#response = requests.put(BASE + "video/1", {"likes": 10, "name": "Mack", "views": 1000})
-#print(response.json())
-#input()
+data = [{"likes": 200, "name": "Mack", "views": 100000},
+        {"likes": 10000, "name": "How to make Rest API", "views": 80000},
+        {"likes": 47, "name": "Ken", "views": 2000}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+
+input()
 response = requests.get(BASE + "video/6")
 print(response.json())
